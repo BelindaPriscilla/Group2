eb7f475186422df954750724408eb95933034bd1
Belinda <belindamk9@gmail.com>
2022-08-17 14:29:52 +0200

    New Version

--- LanternaSolutions/src/main/java/Main2.java
+++ LanternaSolutions/src/main/java/Main2.java
@@ -24,16 +24,17 @@ public class Main2 {
         screen.printToScreen(x, y, player);
         screen.printToScreen(a, b, arrow);
 
-        pirntBoundries();
+        printBoundries();
+        mazeObstacles();
 
-        Position bombPosition = new Position(x,y);
-            for(int i =0; i<15;i++) {
-                Random r = new Random();
-                bombPosition = new Position(r.nextInt(76) +2, r.nextInt(20)+2);
-                if(screen.getChar(bombPosition.x,bombPosition.y)!=block) {
-                    screen.printToScreen(bombPosition.x, bombPosition.y, bomb);
-                }
+        Position bombPosition = new Position(x, y);
+        for (int i = 0; i < 1; i++) {
+            Random r = new Random();
+            bombPosition = new Position(r.nextInt(76) + 2, r.nextInt(20) + 2);
+            if (screen.getChar(bombPosition.x, bombPosition.y) != block) {
+                screen.printToScreen(bombPosition.x, bombPosition.y, bomb);
             }
+        }
 
         // increase size of the Arrow unicode
 
@@ -76,56 +77,31 @@ public class Main2 {
 
 //             ***** more than one if else statement solved
 
-            if (screen.getChar(x, y) == block || screen.getChar(x, y) == block || screen.getChar(x, y) == block) {
+            if (screen.getChar(x, y) == block) {
                 x = oldX;
                 y = oldY;
-            } else {
-//
+            } else if(screen.getChar(x,y)==bomb) {
+                System.out.println("Bang: Game over");
+                screen.close();
+                continueReadingInput = false;
+            }
+            else {
                 screen.printToScreen(oldX, oldY, ' ');
                 screen.printToScreen(x, y, player);
             }
 
 
             Position p = new Position(a, b);
-            for (int i = 0; i < 15; i++) {
-                if ((bombPosition.x == x && bombPosition.y == y) || (p.x == x && p.y == y)) { // add 2nd bomb statement
+                if ((p.x == x && p.y == y)) { // add 2nd bomb statement
                     screen.close();
+                    System.out.println("Congrations! Game over");
                     continueReadingInput = false;
                 }
             }
         }
-    }
 
-        public static void pirntBoundries() throws IOException {
+    public static void printBoundries() throws IOException {
 
-        for (int i = 0; i < 10; i++) {
-            screen.printToScreen(i + 1, 20, block);
-            screen.printToScreen(i + 6, 6, block);
-
-        }
-
-        for (int i = 0; i < 10; i++) {
-            screen.printToScreen(15, i +6, block);//right corner curve
-            //screen.printToScreen(i + 6, 6, block);
-
-        }
-
-        for (int i = 0; i < 6; i++) {
-            screen.printToScreen(i + 30, 16, block);// L shape
-            screen.printToScreen(i + 15, 16, block);// Z Shape
-            screen.printToScreen(i + 24, 10, block);// long T shap
-
-        }
-        for (int i = 0; i < 16; i++){ // 16 to 5 and 5 to16
-            screen.printToScreen(30, i + 1, block); // vertical from horizontal boundar
-        }
-        for (int i = 0; i < 5; i++){ // 16 to 5 and 5 to16
-            screen.printToScreen(36, i + 16, block);
-            //screen.printToScreen(21, i + 19, block);
-        }
-        for (int i = 0; i < 8; i++){ // continuation of z shape
-            screen.printToScreen(21, i + 16, block);
-        }
         for (int i = 0; i < 78; i++) {                          // printing horizontal boundary
             screen.printToScreen(i + 1, 23, block);
             screen.printToScreen(i + 1, 1, block);
@@ -135,23 +111,55 @@ public class Main2 {
             screen.printToScreen(79, i + 1, block);
         }
 
-        for (int i = 0; i < 8; i++) {                          // print 2 vertical boundary on right
-            screen.printToScreen(70, i + 1, block);
-            screen.printToScreen(70, i + 16, block);
-        }
+    }
+        public static void mazeObstacles()throws IOException{
+            for (int i = 0; i < 10; i++) {
+                screen.printToScreen(i + 1, 20, block);
+                screen.printToScreen(i + 6, 6, block);
 
-        for (int i = 0; i < 20; i++) {                          // printing horizontal across vertical boundary
-            screen.printToScreen(i + 59, 11, block);
-            screen.printToScreen(i + 39, 5, block);//long horizontal
-        }
+            }
 
-        for (int i = 0; i < 15; i++) {                          // print 2 vertical crossing horizontal
-            screen.printToScreen(59, i + 5, block);
-            screen.printToScreen(45, i + 9, block);
-        }
+            for (int i = 0; i < 10; i++) {
+                screen.printToScreen(15, i + 6, block);//right corner curve
+                //screen.printToScreen(i + 6, 6, block);
 
-        for (int i = 0; i < 10; i++) {                          // print 2 vertical crossing horizontal
-            screen.printToScreen(39, i + 5, block);
+            }
+
+            for (int i = 0; i < 6; i++) {
+                screen.printToScreen(i + 30, 16, block);// L shape
+                screen.printToScreen(i + 15, 16, block);// Z Shape
+                screen.printToScreen(i + 24, 10, block);// long T shap
+
+            }
+            for (int i = 0; i < 16; i++) { // 16 to 5 and 5 to16
+                screen.printToScreen(30, i + 1, block); // vertical from horizontal boundar
+            }
+            for (int i = 0; i < 5; i++) { // 16 to 5 and 5 to16
+                screen.printToScreen(36, i + 16, block);
+                //screen.printToScreen(21, i + 19, block);
+            }
+            for (int i = 0; i < 8; i++) { // continuation of z shape
+                screen.printToScreen(21, i + 16, block);
+            }
+
+            for (int i = 0; i < 8; i++) {                          // print 2 vertical boundary on right
+                screen.printToScreen(70, i + 1, block);
+                screen.printToScreen(70, i + 16, block);
+            }
+
+            for (int i = 0; i < 20; i++) {                          // printing horizontal across vertical boundary
+                screen.printToScreen(i + 59, 11, block);
+                screen.printToScreen(i + 39, 5, block);//long horizontal
+            }
+            for (int i = 0; i < 15; i++) {                          // print 2 vertical crossing horizontal
+                screen.printToScreen(59, i + 5, block);
+                screen.printToScreen(45, i + 9, block);
+            }
+
+            for (int i = 0; i < 10; i++) {                          // print 2 vertical crossing horizontal
+                screen.printToScreen(39, i + 5, block);
+            }
         }
     }
-}
+
+
